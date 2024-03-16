fa4ed58c8726961438afda83b37196283b93f412
kelzk <liasdfg606@gmail.com>
2024-03-16 19:25:59 +0800

    add: filter, totalNum, findOne api

--- server/index.js
+++ server/index.js
@@ -1,16 +1,53 @@
 import express from "express";
 import cors from "cors";
-import { read } from "./routes/read.js";
+import bodyParser from "body-parser";
+import { read, filter, readTotalNum, findOne } from "./routes/read.js";
 import { client } from "./database.js";
 
 const app = express();
 const port = 3000;
 
 app.use(cors({ origin: "http://localhost:3001" }));
+app.use(bodyParser.json());
 
 app.get("/", async (req, res) => {
-  const result = await read(client);
-  res.send(result);
+  try {
+    const result = await read(client);
+    res.send(result);
+  } catch (error) {
+    console.error("Error querying the database:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/filter", async (req, res) => {
+  try {
+    const result = await filter(client, req.body);
+    res.send(result);
+  } catch (error) {
+    console.error("Error querying the database:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/findOne", async (req, res) => {
+  try {
+    const result = await findOne(client, req.body);
+    res.send(result);
+  } catch (error) {
+    console.error("Error querying the database:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/holdings", async (req, res) => {
+  try {
+    const result = await readTotalNum(client);
+    res.send(result.toString());
+  } catch (error) {
+    console.error("Error querying the database:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 app.listen(port, () => {
--- server/routes/read.js
+++ server/routes/read.js
@@ -1,10 +1,40 @@
 const read = async (client) => {
   await client.connect();
   const db = client.db("RNA-virus");
-  const collection = db.collection("properties");
-  const findResult = collection.find();
+  const collection = db.collection("properties-2");
+  const findResult = collection
+    .find()
+    .project({ _id: 0, name: 1, source: 1, sourceId: 1, type: 1 });
   const result = await findResult.toArray();
   return result;
 };
 
-export { read };
+const filter = async (client, criteria) => {
+  await client.connect();
+  const db = client.db("RNA-virus");
+  const collection = db.collection("properties-2");
+  const findResult = collection
+    .find(criteria)
+    .project({ _id: 0, name: 1, source: 1, sourceId: 1, type: 1 });
+  const result = await findResult.toArray();
+  return result;
+};
+
+const readTotalNum = async (client) => {
+  await client.connect();
+  const db = client.db("RNA-virus");
+  const collection = db.collection("properties-2");
+  const totalNum = collection.countDocuments();
+  return totalNum;
+};
+
+const findOne = async (client, criteria) => {
+  await client.connect();
+  const db = client.db("RNA-virus");
+  const collection = db.collection("properties-2");
+  const findResult = collection.find(criteria);
+  const result = await findResult.toArray();
+  return result;
+};
+
+export { read, filter, readTotalNum, findOne };
