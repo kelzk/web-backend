369be421bb67bb9fa2049f14d4a2779c1abef678
kelzk <liasdfg606@gmail.com>
2024-03-18 19:16:15 +0800

    add: url params

--- server/routes/read.js
+++ server/routes/read.js
@@ -1,9 +1,29 @@
+/* eslint-disable no-restricted-syntax */
 const filter = async (client, criteria) => {
+  console.log(criteria);
+  const query = {};
+  for (const [key, value] of Object.entries(criteria)) {
+    query[key] = { $regex: value, $options: "i" };
+    if (value[0] === "<") {
+      query[key] = { $lt: Number(value.slice(1)) };
+    }
+    if (value[0] === ">") {
+      query[key] = { $gt: Number(value.slice(1)) };
+    }
+    if (value.includes("-")) {
+      const hyphenPos = value.indexOf("-");
+      query[key] = {
+        $gte: Number(value.slice(0, hyphenPos)),
+        $lte: Number(value.slice(hyphenPos + 1)),
+      };
+    }
+  }
+  console.log(query);
   await client.connect();
   const db = client.db("RNA-virus");
   const collection = db.collection("properties-2");
   const findResult = collection
-    .find(criteria)
+    .find(query)
     .project({ _id: 0, name: 1, source: 1, sourceId: 1, type: 1 });
   const result = await findResult.toArray();
   return result;
